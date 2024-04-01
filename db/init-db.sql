c2075e4a162a5dd852577779ea58cccb0065623d
Jang <kinskicom@gmail.com>
2024-04-01 16:32:08 +0900

    webhook test #7

--- db/init-db.sql
+++ db/init-db.sql
@@ -8,8 +8,8 @@ CREATE TABLE users
 INSERT INTO users VALUES(1, 'abc123', 'abc123@abc123.co.kr');
 INSERT INTO users VALUES(2, '987xyz', '987xyz@987xyz.co.kr');
 INSERT INTO users VALUES(3, 'webhook', 'webhook@test.co.kr');;
-INSERT INTO users VALUES(4, 'webhook2', 'webhook2@test2.co.kr');
-INSERT INTO users VALUES(5, 'webhook3', 'webhook3@test2.co.kr');
-INSERT INTO users VALUES(6, 'webhook4', 'webhook4@test2.co.kr');
-INSERT INTO users VALUES(7, 'webhook5', 'webhook5@test2.co.kr');
-INSERT INTO users VALUES(8, 'webhook6', 'webhook6@test5.co.kr');
+INSERT INTO users VALUES(4, 'webhook2', 'webhook123@test2.co.kr');
+INSERT INTO users VALUES(5, 'webhook3', 'webhook3@tes12t2.co.kr');
+INSERT INTO users VALUES(6, 'webhook4', 'webhook4@te45st2.co.kr');
+INSERT INTO users VALUES(7, 'webhook5', 'webhook5@te6st2.co.kr');
+INSERT INTO users VALUES(8, 'webhook6', 'webhook6@te7st5.co.kr');
